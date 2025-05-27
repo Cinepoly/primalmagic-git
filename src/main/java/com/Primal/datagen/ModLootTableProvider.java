7fa2526d84906a455aad637994b41d7df1332ee3
maimai <cixiln1@foxmail.com>
2025-05-27 16:44:09 +0800

    添加fabric数据生成的文件

--- src/main/java/com/Primal/datagen/ModLootTableProvider.java
+++ src/main/java/com/Primal/datagen/ModLootTableProvider.java
@@ -1,9 +1,22 @@
 package com.Primal.datagen;
 
+import com.Primal.block.ModBlocks;
+import com.Primal.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.block.Block;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.Item;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LootPoolEntry;
+import net.minecraft.loot.function.ApplyBonusLootFunction;
+import net.minecraft.loot.function.SetContentsLootFunction;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 
 import java.util.concurrent.CompletableFuture;
@@ -18,5 +31,31 @@ public class ModLootTableProvider extends FabricBlockLootTableProvider {
     @Override
     public void generate() {
 
+        //矿石类掉落物的数据生成
+            addDrop(ModBlocks.ILLUSIONARY_BLOCK,oreDrops(ModBlocks.ILLUSIONARY_BLOCK,
+                    ModItems.ILLUSIONARY_TRANSFORMATION_CURSE));
+            addDrop(ModBlocks.REFINING_STONE_BLOCK,oreDrops(ModBlocks.REFINING_STONE_BLOCK,
+                    ModItems.REFINING_STONE));
+            addDrop(ModBlocks.SPIRITUAL_MEDIUM_STONE_BLOCK,oreDrops(ModBlocks.SPIRITUAL_MEDIUM_STONE_BLOCK,
+                    ModItems.SPIRITUAL_MEDIUM_STONE));
+
+            //工作台类似的其他物品掉落物数据生成
+             addDrop(ModBlocks.MAGIC_BINDING_TABLE);
     }
+
+
+    public LootTable.Builder copperOreLinkDrops(Block drop, Item dropItem) {
+        RegistryWrapper.Impl<Enchantment> impl = this.registryLookup.getWrapperOrThrow(RegistryKeys.ENCHANTMENT);
+        return this.dropsWithSilkTouch(
+                drop,
+                (LootPoolEntry.Builder<?>) this.applyExplosionDecay(
+                        drop,
+                        ItemEntry.builder(dropItem)
+                                .apply(net.minecraft.loot.function.SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0F, 2.0F)))
+                                .apply(ApplyBonusLootFunction.oreDrops(impl.getOrThrow(Enchantments.FORTUNE)))
+                )
+        );
+    }
+
+
 }
--- src/main/java/com/Primal/datagen/ModModelsProvider.java
+++ src/main/java/com/Primal/datagen/ModModelsProvider.java
@@ -1,22 +1,66 @@
 package com.Primal.datagen;
 
+import com.Primal.block.ModBlocks;
+import com.Primal.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.client.BlockStateModelGenerator;
-import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.*;
+import net.minecraft.util.Identifier;
 
-public class ModModelsProvider extends FabricModelProvider{
-    public ModModelsProvider(FabricDataOutput output){
+public class ModModelsProvider extends FabricModelProvider {
+    public ModModelsProvider(FabricDataOutput output) {
         super(output);
     }
-    @Override
-    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
 
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator generator) {
+        // 六面相同
+        generator.registerSimpleCubeAll(ModBlocks.REFINING_STONE_BLOCK);
+        generator.registerSimpleCubeAll(ModBlocks.ILLUSIONARY_BLOCK);
+        generator.registerSimpleCubeAll(ModBlocks.SPIRITUAL_MEDIUM_STONE_BLOCK);
+
+        // 六面不同
+        generator.register(ModBlocks.MAGIC_BINDING_TABLE, context -> {
+            TextureMap textures = new TextureMap()
+                    .put(TextureKey.SIDE, Identifier.of("primalmagic", "block/magic_binding_table_side"))
+                    .put(TextureKey.FRONT, Identifier.of("primalmagic", "block/magic_binding_table_front"))
+                    .put(TextureKey.BACK, Identifier.of("primalmagic", "block/magic_binding_table_back"))
+                    .put(TextureKey.TOP, Identifier.of("primalmagic", "block/magic_binding_table_top"))
+                    .put(TextureKey.BOTTOM, Identifier.of("primalmagic", "block/magic_binding_table_bottom"));
+            Identifier modelId = Models.CUBE.upload(context, textures, generator.modelCollector);
+            return BlockStateModelGenerator.createSingletonBlockState(context, modelId, textures);
+        });
     }
 
     @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        // 物品模型生成
+    itemModelGenerator.register(ModItems.SCHOLAR_SCEPTRE,Models.GENERATED);
+        itemModelGenerator.register(ModItems.MASTER_SCEPTRE,Models.GENERATED);
+        itemModelGenerator.register(ModItems.PASTORAL_SCEPTRE,Models.GENERATED);
+        itemModelGenerator.register(ModItems.ORIGINAL_SCEPTRE,Models.GENERATED);
+        itemModelGenerator.register(ModItems.ORIGINAL_SCEPTRE,Models.GENERATED);
+        itemModelGenerator.register(ModItems.THUNERCLAP_TALISMAN,Models.GENERATED);
+        itemModelGenerator.register(ModItems.FLAME_TALISMAN,Models.GENERATED);
+        itemModelGenerator.register(ModItems.BREEZE_TALISMAN,Models.GENERATED);
+        itemModelGenerator.register(ModItems.FROZEN_TALISMAN,Models.GENERATED);
+        itemModelGenerator.register(ModItems.ROCK_TALISMAN,Models.GENERATED);
+        itemModelGenerator.register(ModItems.ORIGINALSTREAM_TALISMAN,Models.GENERATED);
+        itemModelGenerator.register(ModItems.SPIRITWOOD_TALISMAN,Models.GENERATED);
+        itemModelGenerator.register(ModItems.VASTNESS_ADAMANTROCK,Models.GENERATED);
+        itemModelGenerator.register(ModItems.VASTNESS_GLACIATE,Models.GENERATED);
+        itemModelGenerator.register(ModItems.VASTNESS_STREAM,Models.GENERATED);
+        itemModelGenerator.register(ModItems.VASTNESS_VERDANT,Models.GENERATED);
+        itemModelGenerator.register(ModItems.VASTNESS_ZEPHYR,Models.GENERATED);
+        itemModelGenerator.register(ModItems.VASTNESS_THUNDERCLAP,Models.GENERATED);
+        itemModelGenerator.register(ModItems.REFINING_STONE,Models.GENERATED);
+        itemModelGenerator.register(ModItems.ELEMENTAL_MASTERMIND,Models.GENERATED);
+        itemModelGenerator.register(ModItems.SOUL_OF_MINGYUAN,Models.GENERATED);
+        itemModelGenerator.register(ModItems.SPIRITUAL_MEDIUM_STONE,Models.GENERATED);
+        itemModelGenerator.register(ModItems.ILLUSIONARY_TRANSFORMATION_CURSE,Models.GENERATED);
+        
+
+
 
     }
 }
